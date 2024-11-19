cbf1e8ae6477c2c8f672d3db74f808e0d80349f2
20232ADS.CNT0021 <vitorsampi16@gmail.com>
2024-11-18 21:54:39 -0300

    funcoes

---
+++ index.js
@@ -0,0 +1,1 @@
+import ordenar from "./ordenacao";
---
+++ ordenacao.js
@@ -0,0 +1,13 @@
+function bolha(vetor){
+    console.log("Ordenação em bolha")
+}
+
+function selecao(vetor){
+    console.log("Ordenação em selecao")
+}
+
+function auxiliar(){
+    console.log("Auxiliando!")
+}
+
+export default {bolha, selecao, auxiliar}
